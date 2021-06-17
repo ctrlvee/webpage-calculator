8253d63606b219105dc46d08d063cfc5ddaaa65d
ctrlvee <me123opportunity@gmail.com>
2021-06-17 17:29:01 -0400

    Completed javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// store information that is currently typed up
-console.log("Working 1");
+
 class Calculator { 
+    // store information that is currently typed up
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
@@ -8,37 +8,95 @@ class Calculator {
     }
 
     clear () {
+        // Set values to default value
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
     delete() {
-    
+        this.currentOperand = this.currentOperand.toString().slice(0, -1) // chops off last digit by converting it to string and slicing
     }
 
     appendNumber(number) {
-        this.currentOperand = number
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
 
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return // wont run code if empty
+        if (this.previousOperand !== '') {
+            this.compute()
+        } // if previous operand isn't an empty string, run the compute function
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
 
     }
 
     compute() {
+        console.log("1");
+        let computation
+        const prev = parseFloat(this.previousOperand) // takes the previous operand string and changes it to a number
+        const current = parseFloat(this.currentOperand)
+        // takes the current operand string and changes it to a number
+        if (isNaN(prev) || isNaN(current)) return // if there is no previous value or no current value, it only returns nothing since there are no computations to be done
+        switch (this.operation) { 
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return // don't do anything if none of the operations match
+        }
 
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+        
+    
     // Update values on display
     updateDisplay() { 
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}` // includes the operation sybmol to the previous operand output
+        }
+
+        
     }
 
 
 }
-console.log("Working 2");
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -48,7 +106,7 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-console.log("Working 3");
+
 
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
@@ -56,4 +114,28 @@ numberButtons.forEach(button => {
     calculator.updateDisplay()
   })
 })
-console.log("Working 4");
+
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
